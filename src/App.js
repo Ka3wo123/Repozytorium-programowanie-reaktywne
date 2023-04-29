5d3439d8efb8038499b5cd40832861ec2434291c
Kajetan Nowak <kajtek2n@gmail.com>
2023-04-29 17:08:03 +0200

    Zrealizowano zadanie z laboratorium nr 7

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import QuizComponent from "./components/componentQuiz";
+
+function App() {
+  return (
+    <QuizComponent/>
+  );
+}
+
+export default App;
