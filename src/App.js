785fb4ad3d849dcd9f9291206a6ae1e7d4b424b2
Kajetan Nowak <kajtek2n@gmail.com>
2023-05-05 13:58:34 +0200

    Zrealizowano zadanie z lab8

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Posts from "./components/posts";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="container">
+      <Posts />      
+    </div>
+  );
+}
+
+export default App;
