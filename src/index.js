a60c0bd71602b1ffbc9a71177669527c2ce0d91d
Kajetan Nowak <kajtek2n@gmail.com>
2023-06-04 09:37:59 +0200

    Zrealizowano zadanie z lab10

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+import Posts from "./components/posts";
+import Home from "./components/home";
+import NotFound from "./components/notFound";
+import LoginForm from "./components/loginForm";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App/>}>
+                    <Route path="home" element={<Home/>}/>
+                    <Route path="posts" element={<Posts/>}/>
+                    <Route path="login" element={<LoginForm/>}/>
+                    <Route path="*" element={<NotFound/>}/>
+                </Route>
+
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
